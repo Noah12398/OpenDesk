03f16373635a762957e27ad4bc788ce961c74226
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2026-02-14 18:44:35 +0530

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,16 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://open-desk-tan.vercel.app"
 ];
-
 app.use((req, res, next) => {
   const origin = req.headers.origin;
 
-  if (allowedOrigins.includes(origin)) {
+  if (
+    origin &&
+    (
+      origin.includes("vercel.app") ||
+      origin.includes("localhost")
+    )
+  ) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
@@ -17,7 +22,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
 
-  // HANDLE PREFLIGHT
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
   }
